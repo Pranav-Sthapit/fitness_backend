153b39852d9d819e1ac67b267835ea7d045f9822
Pranav Sthapit <pranavsthapit17@gmail.com>
2026-02-23 14:47:45 +0545

    1.2

--- config/db.js
+++ config/db.js
@@ -3,12 +3,17 @@ const {Pool}=pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool=new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST_CLOUD,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD_CLOUD,
-    port:Number(process.env.DB_PORT),
-    ssl: { rejectUnauthorized: false }, // required for Supabase cloud
-  family: 4
+// export const pool=new Pool({
+//     user:process.env.DB_USER,
+//     host:process.env.DB_HOST_CLOUD,
+//     database:process.env.DB_NAME,
+//     password:process.env.DB_PASSWORD_CLOUD,
+//     port:Number(process.env.DB_PORT),
+//     ssl: { rejectUnauthorized: false }, // required for Supabase cloud
+//   family: 4
+// });
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
